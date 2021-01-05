89c5113a7a67d24fe83eb99c1a3cf425fc7c38be
sintayehu1993 <sintayeh@buffalo.edu>
2021-01-05 14:16:11 -0500

    my back end server is set up right

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/api', require('../DIGITALGIFTCARD/server/apiRoutes'));
+
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+
+
+  const port = process.env.PORT || 1993; // this can be very useful if you deploy to Heroku!
+app.listen(port, function () {
+  console.log("Knock, knock");
+  console.log("Who's there?");
+  console.log(`Your server, listening on port ${port}`);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,13 @@
 
 module.exports = {
     entry: ['./client/main.js', //  your entry point is the main.js in the root of your project folder
-    '@babel/polyfill', // enables async-await
 ],
     mode: 'development',
     output: {
       path: __dirname, // assumes your bundle.js will also be in the root of your project folder
       filename: 'bundle.js'
     },
-    devtool: 'source-maps',
+    devtool: 'source-map',
     module: {
       rules: [
         {
